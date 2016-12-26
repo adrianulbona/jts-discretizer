40fa58f22118dee376dc7998312ce749372fbe1d
Adrian Bona <adrianulbona@gmail.com>
2016-12-27 01:14:24 +0200

    Remove unused annotation

--- src/main/java/io/github/adrianulbona/jts/discretizer/GeometryDiscretizer.java
+++ src/main/java/io/github/adrianulbona/jts/discretizer/GeometryDiscretizer.java
@@ -2,14 +2,12 @@ package io.github.adrianulbona.jts.discretizer;
 
 import ch.hsr.geohash.GeoHash;
 import com.vividsolutions.jts.geom.Geometry;
-import lombok.RequiredArgsConstructor;
 
 import java.util.Set;
 
 /**
  * Created by adrianulbona on 26/12/2016.
  */
-@RequiredArgsConstructor
 public abstract class GeometryDiscretizer<G extends Geometry> {
 
 	public abstract Set<GeoHash> discretize(G geometry);
